37a2e2dd4677439d97fcf87c9a7c5a6535bd8d5e
Sumita Sami <sumita.sami@gmail.com>
2026-05-01 12:00:52 -0700

    Minor commit to trigger redeployment

--- kidwords-web/src/main.tsx
+++ kidwords-web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import App from "./App";
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider value={defaultSystem}>
